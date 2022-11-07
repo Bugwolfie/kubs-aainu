1fb02005ed7ce7150b68305b934d5df5843fcc9f
kabir <kabirhowl@gmail.com>
2022-11-07 14:22:27 +0530

    this one remained so added and pushed

--- pythons_basics/patterns_operators_basicshit/34_another_method_pt.py
+++ pythons_basics/patterns_operators_basicshit/34_another_method_pt.py
@@ -4,9 +4,9 @@ def star(n):
         print()
 star(6)
 
-'''def star2(n1):
+def star2(n1):
     for i in range(0,n1,1):
         print("*"*(n1-i), end= "  ")
         print()
 
-star2(5)'''
+star2(5)
